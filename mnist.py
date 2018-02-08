f677146d27a4e7fe13bbb58878d2cd6a55ee4a8f
Himanshi Singh <singh.himanshi004@gmail.com>
2018-02-08 15:26:49 +0530

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -19,17 +19,16 @@ train=pd.read_csv("mnist_train.csv",header=None)
 test=pd.read_csv("mnist_test.csv",header=None)
 
 #to chck how our data is
-xtrain=train.iloc[:,1:] #0th column is label so removed
-ytrain=train.iloc[:,0] #0thof ytrain will tell you what no. is it
+xtrain=train.iloc[:,1:]        #0th column is label so removed
+ytrain=train.iloc[:,0]         #0th of ytrain will tell us what no. is this actually
 xtest=test.iloc[:,1:] 
 ytest=test.iloc[:,0]
 ytrain5=(ytrain==5).astype(np.int)
 ytest5=(ytest==5).astype(np.int)
 
-arow=xtrain.iloc[0,:].reshape(28,28)  #this will signify one digit, taking one complete row
+arow=xtrain.iloc[0,:].reshape(28,28)       #this will signify one digit in whole, taking one complete row at a time
 plt.imshow(arow,cmap=matplotlib.cm.binary,interpolation='nearest')
 ytrain[0]
-
 
 sgd=linear_model.SGDClassifier(random_state=42)
 sgd.fit(xtrain,ytrain5)
@@ -49,10 +48,7 @@ print(predicted2)
 conf_mat1=metrics.confusion_matrix(ytest5,predicted1)
 print(conf_mat1)
 
-#print precision recall curve and auc curve
+
 #accuracy=96.3%   precission=75.1%  
-#sgd.predict_proba(xtest)  #error: because it doesn't use sigmoid so no probability thing
-#(loss='log',random_state=42) if we write like this then it will use logistic regression and will provide prob ting
-#loss is another name for cost function
 
 
